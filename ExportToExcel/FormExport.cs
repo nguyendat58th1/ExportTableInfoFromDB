a87045e52b45d6113d3dbd297e386630fd779a27
Huy Dat <dat.nguyen4@ntq-solution.com.vn>
2022-04-06 15:20:48 +0700

    Upload DataGridView

--- ExportToExcel/FormExport.cs
+++ ExportToExcel/FormExport.cs
@@ -36,32 +36,17 @@ namespace ExportToExcel
 			//dataGridView.DataSource = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
 		}
 
-		public List<string> PropertiesFromTypeExceptVitualMethod(object atype)
-		{
-			if (atype == null) return new List<string>() { };
-			Type t = atype.GetType();
-			PropertyInfo[] properties = atype.GetType().GetProperties()
-											.Where(p => !p.GetGetMethod().IsVirtual).ToArray();
-			List<string> propNames = new List<string>();
-			foreach (PropertyInfo prp in properties)
-			{
-				Debug.WriteLine(prp.Name);
-				propNames.Add(prp.Name);
-			}
-			return propNames;
-		}
-
 		public List<string> PropertiesFromTypeOnlyVitualMethod(object atype)
 		{
 			if (atype == null) return new List<string>() { };
 			Type t = atype.GetType();
 			PropertyInfo[] properties = atype.GetType().GetProperties()
-											.Where(p => p.GetGetMethod().IsVirtual).ToArray();
+											 .Where(p => p.GetGetMethod().IsVirtual).ToArray();
 			List<string> propNames = new List<string>();
-			foreach (PropertyInfo prp in properties)
+			foreach (PropertyInfo prop in properties)
 			{
-				Debug.WriteLine(prp.Name);
-				propNames.Add(prp.Name);
+				Debug.WriteLine(prop.Name);
+				propNames.Add(prop.Name);
 			}
 			return propNames;
 		}
@@ -74,14 +59,39 @@ namespace ExportToExcel
 				{
 					try
 					{
-						using (XLWorkbook workBook = new XLWorkbook())
+
+						// creating Excel Application  
+						Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+						// creating new WorkBook within Excel application  
+						Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+						// creating new Excelsheet in workbook  
+						Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+						// see the excel sheet behind the program  
+						app.Visible = true;
+						// get the reference of first sheet. By default its name is Sheet1.  
+						// store its reference to worksheet  
+						worksheet = workbook.Sheets["Sheet1"];
+						worksheet = workbook.ActiveSheet;
+						// changing the name of active sheet  
+						worksheet.Name = comboBox.SelectedItem.ToString();
+						// storing header part in Excel  
+						for (int i = 1; i < dataGridView.Columns.Count + 1; i++)
 						{
-							Dao dao = new Dao();
-							var dbSet = dao.GetDbSet(comboBox.SelectedItem.ToString());
-							var props = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
-							workBook.Worksheets.Add(ToDataTable(props), comboBox.SelectedItem.ToString());
-							workBook.SaveAs(sfd.FileName);
+							worksheet.Cells[1, i] = dataGridView.Columns[i - 1].HeaderText;
 						}
+						// storing Each row and column value to excel sheet  
+						for (int i = 0; i < dataGridView.Rows.Count - 1; i++)
+						{
+							for (int j = 0; j < dataGridView.Columns.Count; j++)
+							{
+								worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value.ToString();
+							}
+						}
+						// save the application  
+						workbook.SaveAs(sfd.FileName);
+						// Exit from the application  
+						app.Quit();
+
 						MessageBox.Show("Export successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
 					}
 					catch (Exception ex)
@@ -104,23 +114,22 @@ namespace ExportToExcel
 
 		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			//Dao dao = new Dao();
-			//var dbSet = dao.GetDbSet(comboBox.SelectedItem.ToString());
-			//var props = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
-			//dataGridView.Columns.Clear();
-			//for (int i = 0; i < props.Count(); i++)
-			//{
-			//	dataGridView.Columns.Add(string.Format("col{0}", i), props[i]);
-			//}
 			string connString = "DATA SOURCE=localhost:1521/XE;PASSWORD=ixfds;PERSIST SECURITY INFO=True;USER ID=IXFDS";
-			string sqlRead = $"SELECT COLUMN_NAME, DATA_TYPE, DATA_LENGTH  FROM all_tab_columns WHERE table_name = '{comboBox.SelectedItem.ToString()}'";
+			string sqlRead1 = "SELECT t1.COLUMN_NAME, t1.DATA_TYPE || '(' || t1.DATA_LENGTH || ')' AS DATA_TYPE , t2.COMMENTS  FROM " +
+							"(SELECT COLUMN_NAME, DATA_TYPE, DATA_LENGTH FROM all_tab_columns WHERE table_name = :TABLENAME) t1 " +
+							"JOIN " +
+							"(SELECT COLUMN_NAME, COMMENTS " +
+							"FROM ALL_COL_COMMENTS " +
+							"WHERE OWNER LIKE 'IXFDS' AND TABLE_NAME LIKE :TABLENAME) t2 " +
+							"ON t1.COLUMN_NAME = t2.COLUMN_NAME";
+
 			using (var conn = new OracleConnection(connString))
 			{
 				conn.Open();
-				using (var cmd = new OracleCommand(sqlRead, conn))
+				using (var cmd = new OracleCommand(sqlRead1, conn))
 				{
 					string tableNameParam = comboBox.SelectedItem.ToString();
-					//cmd.Parameters.Add(":TABLENAME", tableNameParam);
+					cmd.Parameters.Add(":TABLENAME", tableNameParam);
 					OracleDataReader odr = cmd.ExecuteReader(); //Read data
 					OracleDataAdapter oda = new OracleDataAdapter(cmd);
 					DataTable data = new DataTable();
