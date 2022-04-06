bc023f44489ad7a69555628c351d9072bc83008c
Huy Dat <dat.nguyen4@ntq-solution.com.vn>
2022-04-06 10:10:40 +0700

    Add project files.

---
+++ Database/Dao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+	public class Dao
+	{
+		NexEntities context = new NexEntities();
+		public DbSet GetDbSet(string tableName)
+		{
+			var type = Assembly.GetExecutingAssembly()
+					.GetTypes()
+					.FirstOrDefault(t => t.Name == tableName);
+			if(type == null)
+			{
+				return null;
+			}
+			DbSet newContext = context.Set(type);
+			return newContext;
+		}
+	}
+}
---
+++ ExportToExcel/FormExport.cs
@@ -0,0 +1,117 @@
+﻿using ClosedXML.Excel;
+using Database;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ExportToExcel
+{
+	public partial class FormExport : Form
+	{
+		private NexEntities context = new NexEntities();
+
+		public FormExport()
+		{
+			InitializeComponent();
+		}
+
+		private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+		}
+
+		private void FormExport_Load(object sender, EventArgs e)
+		{
+			var props = PropertiesFromTypeOnlyVitualMethod(context);
+			foreach (var prop in props)
+			{
+				comboBox.Items.Add(prop);
+			}
+
+			//var dbSet = dao.GetDbSet(comboBox1.SelectedItem.ToString());
+			//dataGridView.DataSource = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
+		}
+
+		public List<string> PropertiesFromTypeExceptVitualMethod(object atype)
+		{
+			if (atype == null) return new List<string>() { };
+			Type t = atype.GetType();
+			PropertyInfo[] properties = atype.GetType().GetProperties()
+											.Where(p => !p.GetGetMethod().IsVirtual).ToArray();
+			List<string> propNames = new List<string>();
+			foreach (PropertyInfo prp in properties)
+			{
+				Debug.WriteLine(prp.Name);
+				propNames.Add(prp.Name);
+			}
+			return propNames;
+		}
+
+		public List<string> PropertiesFromTypeOnlyVitualMethod(object atype)
+		{
+			if (atype == null) return new List<string>() { };
+			Type t = atype.GetType();
+			PropertyInfo[] properties = atype.GetType().GetProperties()
+											.Where(p => p.GetGetMethod().IsVirtual).ToArray();
+			List<string> propNames = new List<string>();
+			foreach (PropertyInfo prp in properties)
+			{
+				Debug.WriteLine(prp.Name);
+				propNames.Add(prp.Name);
+			}
+			return propNames;
+		}
+
+		private void btnExport_Click(object sender, EventArgs e)
+		{
+			using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+			{
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						using (XLWorkbook workBook = new XLWorkbook())
+						{
+							Dao dao = new Dao();
+							var dbSet = dao.GetDbSet(comboBox.SelectedItem.ToString());
+							var props = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
+							workBook.Worksheets.Add(ToDataTable(props), comboBox.SelectedItem.ToString());
+							workBook.SaveAs(sfd.FileName);
+						}
+						MessageBox.Show("Export successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		public static DataTable ToDataTable(List<string> list)
+		{
+			DataTable dataTable = new DataTable();
+			foreach (var col in list)
+			{
+				dataTable.Columns.Add(col);
+			}
+			return dataTable;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Dao dao = new Dao();
+			var dbSet = dao.GetDbSet(comboBox.SelectedItem.ToString());
+			var props = PropertiesFromTypeExceptVitualMethod(dbSet.Create());
+			dataGridView.Columns.Clear();
+			for (int i = 0; i < props.Count(); i++)
+			{
+				dataGridView.Columns.Add(string.Format("col{0}", i), props[i]);
+			}
+
+		}
+	}
+}
