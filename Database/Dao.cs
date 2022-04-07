ad889270e912503d814defafa8ffa12fcfceca0b
Tran Thanh Loc <loc.tran@ntq-solution.com.vn>
2022-04-07 15:21:28 +0700

    Update Column Constrain_Name

--- Database/Dao.cs
+++ Database/Dao.cs
@@ -12,10 +12,13 @@ namespace Database
 			DataTable data = new DataTable();
 			string connString = GetConnectionString();
 			Debug.WriteLine(connString);
-			string sqlRead = "SELECT t1.column_name ,t1.data_type || '(' || t1.data_length || ')' AS DATA_TYPE, t1.NULLABLE, t2.comments " +
-							"FROM all_tab_columns t1, all_col_comments t2 " +
-							"WHERE t1.column_name  = t2.COLUMN_NAME AND t1.TABLE_NAME =  " +
-							":TABLENAME AND t2.TABLE_NAME = :TABLENAME";
+			string sqlRead = "SELECT t1.column_name ,t1.data_type || '(' || t1.data_length || ')' AS DATA_TYPE, t1.NULLABLE, t2.comments, t3.CONSTRAINT_NAME " +
+							 "FROM all_tab_columns t1 " +
+							 "JOIN all_col_comments t2 ON t2.COLUMN_NAME  = t1.column_name " +
+							 "LEFT JOIN " +
+							 "(SELECT * FROM all_cons_columns t WHERE t.table_name  = :TABLENAME AND (t.POSITION IS NOT NULL AND t.constraint_name NOT LIKE 'SYS%')) " +
+							 "t3 ON t3.column_name  = t2.column_name " +
+							 "WHERE t1.table_name = :TABLENAME AND t2.table_name = :TABLENAME";
 
 			using (var conn = new OracleConnection(connString))
 			{
